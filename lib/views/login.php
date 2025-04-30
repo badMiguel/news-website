7b66095fd7f00484239152e03310526413ac5631
Hyein Lee <hyein9792@gmail.com>
2025-05-01 00:37:00 +0930

    create login view

--- lib/views/login.php
+++ lib/views/login.php
@@ -1,1 +1,11 @@
 <h1>login page</h1>
+
+<h2>login HEHE</h2>
+<?php if (isset($error)): ?>
+    <p style="color: red;"><?php echo htmlspecialchars($error); ?></p>
+<?php endif; ?>
+<form method="POST" action="/login">
+    <label>Username: <input type="text" name="username" required></label><br>
+    <label>Password: <input type="password" name="password" required></label><br>
+    <button type="submit">Login</button>
+</form>
